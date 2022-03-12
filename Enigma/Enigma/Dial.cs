bb8cc866feb6a3b9860f963cd517f03dc549b88c
Decker Ayers <69859630+ayersdecker@users.noreply.github.com>
2022-03-11 19:56:46 -0500

    Version 1.1 (3/11/22) Decker R. Ayers (Fencepost Mod 1)

---
+++ Enigma/Enigma/Dial.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Enigma
+{
+    class Dial
+    {
+        public static string DialPlus(int position, string data)
+        {
+            char[] arrData = data.ToCharArray();
+
+            int countOddGreat = 0;
+            int countOddLow = 0;
+
+            int greatIndex = 0;
+            int lowIndex = 0;
+
+            if (arrData.Length % 2 == 0)
+            {
+                countOddGreat = arrData.Length / 2;
+                countOddLow = arrData.Length / 2;
+            }     
+            else
+            {
+                countOddGreat = (int)Math.Ceiling((double)arrData.Length / 2);
+                countOddLow = (int)Math.Floor((double)arrData.Length / 2);
+            }
+            char[] even = new char[countOddGreat];
+            char[] odd = new char[countOddLow];
+
+            for (int round = 0; round < position; round++)
+            {
+
+                for (int i = 0; i < arrData.Length; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        even[greatIndex] = arrData[i];
+                        greatIndex++;
+                    }
+
+                    else
+                    {
+                        odd[lowIndex] = arrData[i];
+                        lowIndex++;
+                    }
+                }
+                for (int i = 0; i < even.Length; i++)
+                {
+                    arrData[i] = even[i];
+                }
+                for (int i = 0; i < odd.Length; i++)
+                {
+                    arrData[i + even.Length] = odd[i];
+                }
+
+                greatIndex = 0;
+                lowIndex = 0;
+            }
+
+            return new string(arrData);
+        }
+        public static string DialMinus(int position, string data)
+        {
+            char[] arrData = data.ToCharArray();
+
+            int countOddGreat = 0;
+            int countOddLow = 0;
+
+            int greatIndex = 0;
+            int lowIndex = 0;
+
+            if (arrData.Length % 2 == 0)
+            {
+                countOddGreat = arrData.Length / 2;
+                countOddLow = arrData.Length / 2;
+            }
+            else
+            {
+                countOddGreat = (int)Math.Ceiling((double)arrData.Length / 2);
+                countOddLow = (int)Math.Floor((double)arrData.Length / 2);
+            }
+            char[] even = new char[countOddGreat];
+            char[] odd = new char[countOddLow];
+
+            for (int round = 0; round < position; round++)
+            {
+                for (int i = 0; i < even.Length; i++)
+                {
+                    even[i] = arrData[i] ;
+                }
+                for (int i = 0; i < odd.Length; i++)
+                {
+                     odd[i] = arrData[i + even.Length];
+                }
+
+                for (int i = 0; i < arrData.Length; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        arrData[i] = even[greatIndex];
+                        greatIndex++;
+                    }
+
+                    else
+                    {
+                        arrData[i] = odd[lowIndex];
+                        lowIndex++;
+                    }
+                }
+                
+
+                greatIndex = 0;
+                lowIndex = 0;
+            }
+
+            return new string(arrData);
+        }
+    }
+}
---
+++ Enigma/Enigma/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Enigma
+{
+    class Program
+    {
+        static string[] mainMenuSelections = new string[] { "Encrypt New Data", "Decrypt Existing Data", "Exit" };
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                switch (Utility.MenuBuild("Main Menu", mainMenuSelections))
+                {
+                    case 1:
+                        CreateEncryption();
+                        break;
+                    case 2:
+                        DecypherData();
+                        break;
+                    case 3:
+                        Utility.ExitProgram();
+                        break;
+                    default:
+                        Utility.TextColor(ConsoleColor.Red, "System Switch Error -- Exiting");
+                        Utility.ExitProgram();
+                        break;
+                }
+            }
+        }
+        private static void CreateEncryption()
+        {
+            Console.WriteLine("Dial 1 [Pick a number 0-9]");
+            int dial1 = Utility.ValidateNum(0, 9);
+            Console.WriteLine("Dial 2 [Pick a number 0-9]");
+            int dial2 = Utility.ValidateNum(0, 9);
+            Console.WriteLine("Dial 3 [Pick a number 0-9]");
+            int dial3 = Utility.ValidateNum(0, 9);
+            
+            string dataIn = Utility.GatherString("Data to be encrypted: ");
+
+            string output = Dial.DialPlus(dial1, dataIn);
+            string output2 = Dial.DialPlus(dial2, output);
+            string output3 = Dial.DialPlus(dial3, output2);
+            Console.WriteLine("\nCoded Text:\n" + output3);
+
+
+
+
+
+            Console.ReadLine();
+        }
+        private static void DecypherData()
+        {
+            Console.Write("Dial 1 ");
+            int dial1 = Utility.ValidateNum(0, 9);
+            Console.Write("Dial 2 ");
+            int dial2 = Utility.ValidateNum(0, 9);
+            Console.Write("Dial 3 ");
+            int dial3 = Utility.ValidateNum(0, 9);
+
+            string dataIn = Utility.GatherString("Data to be decrypted: ");
+            
+            string decode = Dial.DialMinus(dial3, dataIn);
+            string decode2 = Dial.DialMinus(dial2, decode);
+            string decode3 = Dial.DialMinus(dial1, decode2);
+            Console.WriteLine("\nDecoded Text:\n" + decode3);
+
+            Console.ReadLine();
+        }
+    }
+}
